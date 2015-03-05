bd66897e921824cf50dcb1eed78319dfe6104f07
jibaiga0 <jibaigao@tutanota.de>
2015-03-05 19:25:45 -0300

    learnvim site

---
+++ js/loader.js
@@ -0,0 +1,40 @@
+$("#whystart").click(function() {
+                $("#contentArea").load("pages/whystart.html div");
+            });
+/*whystart loading script*/
+
+$("#first").click(function() {
+                $("#contentArea").load("pages/first.html div");
+            });
+/*first chapter loading script*/
+
+	$("#firstsecondnoshit").click(function() {
+  	              $("#contentArea").load("pages/noshit/firstsecondnoshit.html div");
+   	         });
+	/*first chapter no shit version loading script*/
+
+$("#second").click(function() {
+                $("#contentArea").load("pages/second.html div");
+            });
+/*second chapter loading script*/
+
+$("#third").click(function() {
+                $("#contentArea").load("pages/third.html div");
+            });
+/*third chapter loading script*/
+
+$("#forth").click(function() {
+                $("#contentArea").load("pages/forth.html div");
+            });
+/*forth chapter loading script*/
+
+$("#fifth").click(function() {
+                $("#contentArea").load("pages/fifth.html div");
+            });
+/*fifth chapter loading script*/
+
+$("#colornumber").click(function() {
+                $("#contentArea").load("pages/colornumber.html div");
+            });
+/*color number loading script*/
+
