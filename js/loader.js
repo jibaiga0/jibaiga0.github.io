dd221ae2dcd65799bdcacd7355bd20bf07fbbc46
jibai0 <jibaigao@tutanota.de>
2015-03-06 14:17:07 +0100

    some js changes

--- js/loader.js
+++ js/loader.js
@@ -1,16 +1,25 @@
 $("#whystart").click(function() {
                 $("#contentArea").load("pages/whystart.html div");
+                $("html,body").animate({
+                    scrollTop:0
+                },1000);
             });
 /*whystart loading script*/
 
 $("#first").click(function() {
                 $("#contentArea").load("pages/first.html div");
-            });
+                $("html,body").animate({
+                    scrollTop:0
+                },1000);
+   });
 /*first chapter loading script*/
 
 	$("#firstsecondnoshit").click(function() {
   	              $("#contentArea").load("pages/noshit/firstsecondnoshit.html div");
-   	         });
+   	         $("html,body").animate({
+                    scrollTop:0
+                },1000);
+});
 	/*first chapter no shit version loading script*/
 
 $("#second").click(function() {
